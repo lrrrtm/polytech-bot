6e369918eb889d14d0a81a5f51c64b31c4694001
Honor (Artem) <lrrrtm.edu@yandex.ru>
2024-08-21 21:45:18 +0200

    settings design confirmed

--- control_panel/settings_app.py
+++ control_panel/settings_app.py
@@ -1,112 +1,265 @@
+import os
+from urllib.parse import urlparse, parse_qs
+
 import flet as ft
 from dotenv import load_dotenv
 
 from models.database import Database
+from models.redis_s import Redis
 
 load_dotenv()
 
 
 def main(page: ft.Page):
-    page.title = "Настройки бота"
-
-    page.window_width = 390
-    page.window_height = 844
+    page.title = "Политехник"
 
     page.scroll = ft.ScrollMode.AUTO
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
 
-    page.theme = ft.Theme(
-    )
-
-    notification_changed_snack_bar = ft.SnackBar(
-        content=ft.Text(size=16)
-    )
-    page.overlay.append(notification_changed_snack_bar)
-
     db = Database()
+    rd = Redis()
 
-    def notifications_switched(e: ft.ControlEvent):
-        statuses = {
+    def get_card_title(text: str) -> ft.Text:
+        return ft.Text(
+            value=text,
+            size=20,
+            weight=ft.FontWeight.W_400
+        )
+
+    def set_user_data(user_data: dict):
+        user_name.value = user_data['general']['name']
+        user_group.value = user_data['general']['group']
+        user_tid.value = user_data['general']['tid']
+
+        schedule_update_sw.value = user_data['notifications']['schedule_update']
+        lesson_reminder_sw.value = user_data['notifications']['lesson_reminder']
+        service_msg_sw.value = user_data['notifications']['service_message']
+
+    def check_access_token(key: str, value: str):
+        original = rd.get_value(key)
+        if original is None:
+            status = 'empty'
+        else:
+            original = original.decode('utf-8')
+            if original == value:
+                status = 'exists'
+            elif original != value:
+                status = 'wrong'
+
+        return status
+
+    def notification_switch_changed(e: ft.ControlEvent):
+        switch_statuses = {
             True: {
                 'caption': "Уведомление подключено",
-                'color': ft.colors.GREEN
+                'bgcolor': ft.colors.GREEN
             },
             False: {
                 'caption': "Уведомление отключено",
-                'color': ft.colors.AMBER
+                'bgcolor': ft.colors.AMBER
             }
         }
 
-        # switch to db
-        # send message
-        data = db.get_user_notifications_statuses(tid=409801981)
-        print(data.service_msgs, data.schedule_notify, data.schedule_corrections)
-
-        notification_changed_snack_bar.content.value = statuses[e.control.value]['caption']
-        notification_changed_snack_bar.bgcolor = statuses[e.control.value]['color']
-        notification_changed_snack_bar.open = True
+        page.snack_bar = ft.SnackBar(
+            content=ft.Text(
+                size=16,
+                value=switch_statuses[e.control.value]['caption'],
+            ),
+            bgcolor=switch_statuses[e.control.value]['bgcolor']
+        )
+        page.snack_bar.open = True
         page.update()
 
-    page.add(
-        ft.Column(
-            [
-                ft.Column(
-                    [
-                        ft.Text("Основные", size=25),
-                        ft.TextField(label="Имя", hint_text="Введи своё имя", value="Артём",
-                                     prefix_icon=ft.icons.ACCOUNT_CIRCLE),
-                        ft.TextField(label="Группа", hint_text="Введи номер группы", value="5130904/20002",
-                                     prefix_icon=ft.icons.GROUPS),
-                    ]
-                ),
-                ft.Column(
-                    [
-                        ft.Text("Уведомления", size=25),
-                        ft.Container(
-                            ft.Column(
-                                [
-                                    ft.ListTile(
-                                        leading=ft.Switch(
-                                            value=False,
-                                            active_color="#38b349",
-                                            data={'type': 'schedule_corrections'},
-                                            on_change=notifications_switched
-                                        ),
-                                        title=ft.Text("Изменения в расписании"),
-                                        subtitle=ft.Text(
-                                            "Напишем тебе, если в расписании на ближайшую неделю что-то поменяется")
-                                    ),
-                                    ft.ListTile(
-                                        leading=ft.Switch(
-                                            value=False,
-                                            active_color="#38b349",
-                                            data={'type': 'schedule_notify'},
-                                            on_change=notifications_switched
-                                        ),
-                                        title=ft.Text("Следующая пара"),
-                                        subtitle=ft.Text(
-                                            "Отправим информацию о следующей паре за 15 минут до её начала")
-                                    ),
-                                    ft.ListTile(
-                                        leading=ft.Switch(
-                                            value=False,
-                                            active_color="#38b349",
-                                            data={'type': 'service_msgs'},
-                                            on_change=notifications_switched
-                                        ),
-                                        title=ft.Text("Технические сообщения"),
-                                        subtitle=ft.Text(
-                                            "Расскажем о новом функционале бота и предупредим о временном отключении")
-                                    )
-                                ]
+    def access_to_change_data(e: ft.ControlEvent):
+        type = e.control.data
+
+        e.control.icon = ft.icons.SAVE
+        e.control.tooltip = "Сохранить"
+        e.control.data = f"{e.control.data}_save"
+
+        if type == "name":
+            user_name.read_only = False
+            user_name.focus()
+        elif type == "name_save":
+            user_name.read_only = True
+            e.control.icon = ft.icons.EDIT
+            e.control.tooltip = "Изменить имя"
+
+            page.snack_bar = ft.SnackBar(
+                content=ft.Text("Имя изменено"),
+                bgcolor=ft.colors.GREEN
+            )
+            page.snack_bar.open = True
+
+        page.update()
+
+
+    def show_info_dialog(text: str, show_btn: bool = True):
+        page.dialog = ft.AlertDialog(
+            modal=True,
+            title=get_card_title("Настройки"),
+            content=ft.Text(
+                value=text,
+                size=16
+            )
+        )
+        page.dialog.open = True
+        page.update()
+
+    user_name = ft.TextField(
+        icon=ft.icons.ACCOUNT_CIRCLE,
+        label="Имя",
+        read_only=True
+    )
+
+    user_group = ft.TextField(
+        icon=ft.icons.GROUP,
+        label="Учебная группа",
+        read_only=True
+    )
+
+    user_tid = ft.TextField(
+        icon=ft.icons.TELEGRAM,
+        label="Telegram ID",
+        read_only=True
+    )
+
+    general_info = ft.Card(
+        content=ft.Container(
+            content=ft.Column(
+                controls=[
+                    get_card_title("Основное"),
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=user_name,
+                                expand=True
                             ),
-                            padding=ft.padding.only(left=-15)
-                        )
-                    ]
-                ),
-            ]
+                            ft.IconButton(
+                                icon=ft.icons.EDIT,
+                                tooltip="Изменить имя",
+                                data="name",
+                                on_click=access_to_change_data
+                            )
+                        ],
+                        width=page.width
+                    ),
+                    ft.Row(
+                        controls=[
+                            ft.Container(
+                                content=user_group,
+                                expand=True
+                            ),
+                            ft.IconButton(
+                                icon=ft.icons.EDIT,
+                                tooltip="Изменить группу",
+                                on_click=None
+                            )
+                        ],
+                        width=page.width
+                    ),
+                    user_tid
+                ]
+            ),
+            padding=15
         )
     )
+
+    switch_active_color = ft.colors.GREEN
+    schedule_update_sw = ft.Switch(active_color=switch_active_color, on_change=notification_switch_changed,
+                                   data='schedule_update')
+    lesson_reminder_sw = ft.Switch(active_color=switch_active_color, on_change=notification_switch_changed,
+                                   data='lesson_reminder')
+    service_msg_sw = ft.Switch(active_color=switch_active_color, on_change=notification_switch_changed,
+                               data='service_message')
+
+    notifications = ft.Card(
+        content=ft.Container(
+            content=ft.Column(
+                controls=[
+                    get_card_title("Уведомления"),
+                    ft.Container(
+                        content=ft.Column(
+                            controls=[
+                                ft.ListTile(
+                                    leading=schedule_update_sw,
+                                    title=ft.Text("Изменения в расписании"),
+                                    subtitle=ft.Text(
+                                        "Напишем тебе, если в расписании на ближайшие 7 дней что-то поменяется")
+                                ),
+                                ft.ListTile(
+                                    leading=lesson_reminder_sw,
+                                    title=ft.Text("Следующая пара"),
+                                    subtitle=ft.Text(
+                                        "Отправим информацию о следующей паре за 15 минут до её начала")
+                                ),
+                                ft.ListTile(
+                                    leading=service_msg_sw,
+                                    title=ft.Text("Технические сообщения"),
+                                    subtitle=ft.Text(
+                                        "Расскажем о новом функционале и предупредим о временном отключении бота")
+                                )
+                            ]
+                        ),
+                        padding=ft.padding.only(left=-15)
+                    )
+
+                ]
+            ),
+            padding=15
+        )
+    )
+
+    settings_col = ft.Column(
+        controls=[
+            general_info,
+            notifications
+        ]
+    )
+
+    if bool(os.getenv(('DEVMODE'))):
+        page.window.width = 390
+        page.window.height = 844
+        tid = 409801981
+
+    page.route = f"/settings?tid={409801981}&token={'devmode'}"
+    parsed_url = urlparse(page.route)
+    params = parse_qs(parsed_url.query)
+
+    tid = params.get('tid', [None])[0]
+    token = params.get('token', [None])[0]
+
+    token_status = check_access_token(tid, token)
+    if token_status == 'exists':
+        user = db.get_user_by_tid(int(tid))
+        notifications_data = db.get_user_notifications_statuses(int(tid))
+
+        user_data = {
+            'general': {
+                'name': user.name,
+                'group': user.group,
+                'tid': user.tid
+            },
+            'notifications': {
+                'schedule_update': bool(notifications_data.schedule_corrections),
+                'lesson_reminder': bool(notifications_data.schedule_notify),
+                'service_message': bool(notifications_data.service_msgs)
+            }
+        }
+
+        set_user_data(user_data)
+        page.add(settings_col)
+
+    elif token_status == 'wrong':
+        show_info_dialog(
+            text="У вас отсутствует разрешение на просмотр настроек данного пользователя"
+        )
+
+    elif token_status == 'empty':
+        show_info_dialog(
+            text="Срок токена соединения истёк, нажми на кнопку «Настройки» в боте ещё раз."
+        )
+        print('TOKEN STATUS EMPTY')
 
     page.update()
 
