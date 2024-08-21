6b3572cb9cd6ef5080780276986e9d11d95effb8
Honor (Artem) <lrrrtm.edu@yandex.ru>
2024-08-21 19:56:07 +0200

    added kb for webapp

--- bot/keyboards/menu.py
+++ bot/keyboards/menu.py
@@ -1,5 +1,7 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
+from os import getenv
+
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 
 from bot.lexicon import buttons
 
@@ -19,6 +21,19 @@ def get_menu_kb() -> ReplyKeyboardMarkup:
         KeyboardButton(text=buttons.lexicon['menu_next']),
     )
     return builder.as_markup(resize_keyboard=True)
+
+
+def get_settings_btn_kb(user_id: int, access_token: str) -> InlineKeyboardMarkup:
+    builder = InlineKeyboardBuilder()
+
+    builder.button(
+        text="Открыть найстройки",
+        web_app=WebAppInfo(
+            url=f"{getenv('CONTROL_PANEL_URL')}/settings?uid={user_id}&token={access_token}"
+        )
+    )
+
+    return builder.as_markup()
 
 
 def get_back_btn_kb() -> ReplyKeyboardMarkup:
