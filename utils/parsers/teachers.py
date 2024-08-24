41bd950437c47e9ddce83cddf52b438b7c0acae3
Honor (Artem) <lrrrtm.edu@yandex.ru>
2024-08-24 16:24:16 +0200

    teachers list parsing logic

--- utils/parsers/teachers.py
+++ utils/parsers/teachers.py
@@ -12,13 +12,14 @@ def parse_teachers_list():
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920x1080")
 
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
 
     data = []
 
     try:
         driver.get('https://ruz.spbstu.ru/search/teacher?q=%20')
-        while True:
+        pages_count = int(driver.find_elements(By.CLASS_NAME, 'pager__text')[0].text.split()[-1])
+        for _ in range(pages_count - 1):
             teachers = driver.find_elements(By.CLASS_NAME, 'search-result__link')
             for teacher in teachers:
                 teacher_data = {
@@ -26,12 +27,8 @@ def parse_teachers_list():
                     'id': teacher.get_attribute('href').split('/')[-1]
                 }
                 data.append(teacher_data)
-
-            try:
-                next_button = driver.find_element(By.CLASS_NAME, 'fa.fa-arrow-circle-right')
-                next_button.click()
-            except Exception as e:
-                break
+            next_button = driver.find_element(By.CLASS_NAME, 'fa.fa-arrow-circle-right')
+            next_button.click()
         return data
 
     finally:
