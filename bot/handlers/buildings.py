d20c5fea5ee5158bfc0431d35c0f129fb3da6223
Ivan Simonenko <simonenkoivansimon@gmail.com>
2024-08-27 22:03:00 +0400

    completed buildings logic

--- bot/handlers/buildings.py
+++ bot/handlers/buildings.py
@@ -4,10 +4,9 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 
 from bot.handlers.menu import cmd_menu
-from bot.keyboards.menu import get_back_btn_kb
-from bot.lexicon import phrases, buttons
+from bot.keyboards.buildings import get_buildings_kb, get_building_inline_kb
+from bot.lexicon import buttons
 from bot.states.buildings import BuildingsItem
-from bot.keyboards.buildings import get_buildings_kb, get_building_action_kb
 
 from models.database import Database
 
@@ -23,35 +22,25 @@ async def back_btn_to_main_menu(message: Message, state: FSMContext):
     await cmd_menu(message, state)
 
 
-@router.message(BuildingsItem.waiting_for_action, F.text == buttons.lexicon['back_btn'])
-async def back_btn_to_buildings(message: Message, state: FSMContext):
-    await message.answer(
-        text="Выбор действия с корпусом отменён.",
-    )
-    await menu_buildings(message, state)
-
-
 @router.message(Command("buildings"))
 @router.message(F.text == buttons.lexicon['menu_buildings'])
 async def menu_buildings(message: Message, state: FSMContext):
     await state.set_state(BuildingsItem.waiting_for_building)
+    buildings_list = db.get_all_places_names()
     await message.answer(
         text="<b>Выберете корпус:</b>",
-        reply_markup=get_buildings_kb(),
+        reply_markup=get_buildings_kb(buildings_list),
     )
 
 
 @router.message(BuildingsItem.waiting_for_building)
 async def msg_building(message: Message, state: FSMContext):
     try:
-        """image_from_url = URLInputFile(f'{db.get_building_image_url(message.text)}')
-        await message.answer_photo(
-            image_from_url,
-            # caption="Изображение по ссылке"
-        )"""
-        kb = get_building_action_kb(message.text)
+        place_info = db.get_place_info(message.text)
+        kb = get_building_inline_kb(place_info)
         await message.answer(
-            text=f"<b>{message.text}</b>",
+            text=f"<b>{message.text}</b>\n\n"
+                 f"{place_info.caption}",
             reply_markup=kb
         )
     except Exception as e:
--- bot/keyboards/buildings.py
+++ bot/keyboards/buildings.py
@@ -2,38 +2,29 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
 
 from bot.lexicon import buttons
-from bot.lexicon.phrases import lexicon
 
-from models.database import Database
-
-db = Database()
+from models.database import Location
 
 
-def get_buildings_kb() -> ReplyKeyboardMarkup:
+def get_buildings_kb(buildings_list: list[str]) -> ReplyKeyboardMarkup:
     builder = ReplyKeyboardBuilder()
-    buildings_list = db.get_all_places_names()
 
     for item in buildings_list:
         builder.add(KeyboardButton(text=item))
     builder.adjust(2)
     builder.add(KeyboardButton(text=buttons.lexicon['back_btn']))
 
-    return builder.as_markup(one_time_keyboard=True, resize_keyboard=True)
+    return builder.as_markup(resize_keyboard=True)
 
 
-def get_building_action_kb(building_name: str) -> InlineKeyboardMarkup:
+def get_building_inline_kb(place_info: Location) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
-    yandex_building_id = db.get_building_map_id(building_name)
-
-    builder.row(InlineKeyboardButton(text="Маршрут", url=f'https://yandex.ru/maps/-/{yandex_building_id[-1]}'))
+    builder.row(InlineKeyboardButton(text="Маршрут", url=f'https://yandex.ru/maps/-/{place_info.yandex_maps_id}'))
     # todo: сейчас блок обработки ошибок, но в будущем когда в базе будут необходимые поля данных,
-    #  то и заменить блок на if not None:
+    #  то и заменить блок на if not None (добавить проверку в методы Database):
     try:
-        inside_map_url = db.get_building_inside_map_url(building_name)
-        builder.row(InlineKeyboardButton(text="Карта корпуса", url=inside_map_url))
+        builder.row(InlineKeyboardButton(text="Карта корпуса", url=place_info.inside_map))
     except Exception as e:
         print(f"Ошибка при попытке вернуть поле inside_map в базе данных: {e}")
-    # builder.row(InlineKeyboardButton(text=buttons.lexicon['back_btn'], callback_data='back_btn'))
-    # после тестов понял, что инлайн кнопка назад не нужна. Нужна только на реплай кб
 
     return builder.as_markup()
